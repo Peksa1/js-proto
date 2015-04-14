90d61ffde821564ff5e45d512179099fc0603238
Pekka Siika-aho <pekka@siika-aho.fi>
2015-04-14 13:19:33 +0300

    Console logs all of metros currently in traffic and their locations

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
 $(document).ready(function() {
-	$(json).load("http://dev.hsl.fi/siriaccess/vm/json?operatorRef=HSL&lineRef=1300V");
-	$(json).load("http://dev.hsl.fi/siriaccess/vm/json?operatorRef=HSL&lineRef=1300M");
+
+	jQuery.getJSON("http://dev.hsl.fi/siriaccess/vm/json?operatorRef=HSL&lineRef=1300V", function(data) {
+		
+		//console.log(JSON.stringify(data.Siri.ServiceDelivery.VehicleMonitoringDelivery[0].VehicleActivity, null, 4))
+
+		$.each(data.Siri.ServiceDelivery.VehicleMonitoringDelivery[0].VehicleActivity, function(i,v) {
+			console.log(v.MonitoredVehicleJourney.VehicleRef.value + 
+				": longitude: " + v.MonitoredVehicleJourney.VehicleLocation.Longitude +
+				", latitude: " + v.MonitoredVehicleJourney.VehicleLocation.Latitude );
+		});
+	});
+
+
+	jQuery.getJSON("http://dev.hsl.fi/siriaccess/vm/json?operatorRef=HSL&lineRef=1300M", function(data) {
+		
+		//console.log(JSON.stringify(data.Siri.ServiceDelivery.VehicleMonitoringDelivery[0].VehicleActivity, null, 4))
+
+		$.each(data.Siri.ServiceDelivery.VehicleMonitoringDelivery[0].VehicleActivity, function(i,v) {
+			console.log(v.MonitoredVehicleJourney.VehicleRef.value + 
+				": longitude: " + v.MonitoredVehicleJourney.VehicleLocation.Longitude +
+				", latitude: " + v.MonitoredVehicleJourney.VehicleLocation.Latitude );
+		});
+	});
+	//$(json).load("http://dev.hsl.fi/siriaccess/vm/json?operatorRef=HSL&lineRef=1300V");
+	//$(json).load("http://dev.hsl.fi/siriaccess/vm/json?operatorRef=HSL&lineRef=1300M");
 })
