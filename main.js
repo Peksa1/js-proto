99478b50800c011fc8127d8891cb43f91fc8add9
Pekka Siika-aho <pekka@siika-aho.fi>
2015-04-14 12:37:22 +0300

    Index.html shows all of the metro trains currently in operation

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
-	$(json).load("http://dev.hsl.fi/siriaccess/vm/json?operatorRef=HSL");	
+	$(json).load("http://dev.hsl.fi/siriaccess/vm/json?operatorRef=HSL&lineRef=1300V");
+	$(json).load("http://dev.hsl.fi/siriaccess/vm/json?operatorRef=HSL&lineRef=1300M");
 })
